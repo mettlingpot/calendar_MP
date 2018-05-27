ed64dd2b02e47eff44e07ed2cbc199b7c5aa51df
mettlingpot <mettlingpot@hotmail.com>
2018-05-27 22:04:34 +0200

    calendar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,72 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+registerLocaleData(localeFr);
+import { NavController, ModalController, AlertController } from 'ionic-angular';
+import * as moment from 'moment';
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  currentEvents = [];
+  eventSource = [];
+  viewTitle: string;
+  selectedDay = new Date();
+  event = { startTime: new Date().toISOString(), endTime: new Date().toISOString(), allDay: false, title: "test" };
+  calendar = {
+    locale: 'fr-FR',
+    mode: 'month',
+    currentDate: new Date()
+  };
+  
+  constructor(public navCtrl: NavController, private modalCtrl: ModalController, private alertCtrl: AlertController) { 
 
-  constructor(public navCtrl: NavController) {
+console.log(this.eventSource,new Date(),event);  }
 
+   loadEvents() {
+    
   }
 
+  addEvent() {
+    let modal = this.modalCtrl.create('EventModalPage', {selectedDay: this.selectedDay});
+    modal.present();
+    modal.onDidDismiss(data => {
+      if (data) {
+        let eventData = data;
+ 
+        eventData.startTime = new Date(data.startTime);
+        eventData.endTime = new Date(data.endTime);
+        let events = this.eventSource;
+        events.push(eventData);
+        this.eventSource = [];
+         console.log(eventData);
+
+        setTimeout(() => {
+          this.eventSource = events;
+        });
+      }
+    });
+  }
+ 
+  onViewTitleChanged(title) {
+    this.viewTitle = title;
+  }
+ 
+  onEventSelected(event) {
+    let start = moment(event.startTime).format('LLLL');
+    let end = moment(event.endTime).format('LLLL');
+    
+    let alert = this.alertCtrl.create({
+      title: '' + event.title,
+      subTitle: 'From: ' + start + '<br>To: ' + end,
+      buttons: ['OK']
+    })
+    alert.present();
+  }
+ 
+  onTimeSelected(ev) {
+    this.selectedDay = ev.selectedTime;
+  }
 }
